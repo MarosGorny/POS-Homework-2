7899c5055426c8e88d17d75d9beaf71171b4f098
Maroš Gorný <maros.gorny@gmail.com>
2022-12-14 00:27:41 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/DomacaUloha_SAD.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DomacaUloha_SAD.dir/main.c.o"
+  "DomacaUloha_SAD"
+  "DomacaUloha_SAD.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DomacaUloha_SAD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
